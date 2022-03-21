7cda04bafa30aa7f2efdb870a9fade383a71549d
manuel.gonzalez <manuel.gonzalez@houseoftest.se>
2022-03-21 12:20:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HoT_Hello_World_Csharp_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@" _    _      _ _        __          __        _     _ _ ");
+            Console.WriteLine(@"| |  | |    | | |       \ \        / /       | |   | | |");
+            Console.WriteLine(@"| |__| | ___| | | ___    \ \  /\  / /__  _ __| | __| | |");
+            Console.WriteLine(@"|  __  |/ _ \ | |/ _ \    \ \/  \/ / _ \| '__| |/ _` | |");
+            Console.WriteLine(@"| |  | |  __/ | | (_) |    \  /\  / (_) | |  | | (_| |_|");
+            Console.WriteLine(@"|_|  |_|\___|_|_|\___/      \/  \/ \___/|_|  |_|\__,_(_)");
+
+
+        }
+    }
+}
